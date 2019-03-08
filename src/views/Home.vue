af5a74d3cc2bf314f017dc0472cc1d13d0783b5e
Evy <evelynpinho@gmail.com>
2019-03-08 13:28:31 -0300

    Update home layout using flexbox

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,56 @@
 <template>
   <div class="content">
-    <h1>Sallve Movies</h1>
-    <form @submit.prevent="searchMovie" id="registeroffice">
-      <label for="name">nome do filme</label>
-      <input type="text" v-model="form.movie"  placeholder="Find a movie">
-      <button type="submit">Buscar</button>
-    </form>
-    <section class="movie">
-      {{ movie }}
-
+    <section v-if="Object.entries(movie).length === 0">
+      <h1>Sallve Movies</h1>
+      <form @submit.prevent="searchMovie" id="registeroffice">
+        <input type="text" v-model="form.movieName" placeholder="Find a movie">
+        <label for="name">nome do filme</label>
+        <button type="submit">Buscar</button>
+      </form>
     </section>
 
+    <section class="movie" v-else :aria-busy="loading ? 'true' : 'false'">
+      <div class="movie__header">
+        <div class="movie__title">
+            <h2>{{ movie.Title }}</h2>
+        </div>
+        <div class="movie__subtitle">
+            <p>{{ movie.Released }}</p>
+            <p>{{ movie.Genre }}</p>
+        </div>
+      </div>
+
+
+      <div class="movie__body">
+        <div class="movie__img">
+          <img :src="movie.Poster">
+        </div>
+
+        <div class="movie__text">
+          <h3>Plot</h3>
+          <p>{{ movie.Plot }}</p>
+          <h3>Actors</h3>
+          <p>{{ movie.Actors }}</p>
+          <h3>Director</h3>
+          <p>{{ movie.Director }}</p>
+          <h3>Writer</h3>
+          <p>{{ movie.Writer }}</p>
+
+          <div class="movie__ratings">
+            <h3>Ratings</h3>
+              <div v-for="(Ratings, index) in movie.Ratings" :key="index">
+                <h4>{{ Ratings.Source }}</h4>
+                <p>{{ Ratings.Value }}</p>
+              </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="movie__footer">
+        <button @click="cleanMovie">Voltar</button>
+      </div>
+
+    </section>
   </div>
 </template>
 <script>
@@ -19,25 +59,28 @@ export default {
     data() {
       return {
         form: {
-          movie: '',
+          movieName: '',
         },
-        submitLoading: false,
+        loading: false,
       };
     },
   computed: {
     movie() {
-      return this.$store.state.movie;
+      return this.$store.state.movie || lala;
     },
   },
   methods: {
-    toggleSubmitLoading() {
-      this.submitLoading = !this.submitLoading;
+    toggleLoading() {
+      this.loading = !this.loading;
+    },
+    cleanMovie() {
+      this.$store.commit('SET_MOVIE', {});
     },
     searchMovie() {
       const payload = {};
 
-      if (this.form.movie) {
-        payload.movie = this.form.movie;
+      if (this.form.movieName) {
+        payload.movie = this.form.movieName;
       }
       this.$store.dispatch('GET_MOVIE', payload)
         .then(() => {
